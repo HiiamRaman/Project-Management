b962f9ebda67397f666c36e21c8c40536f4a6083
Raman Singh <ramansinghchauhan787@gmail.com>
2025-12-06 15:23:48 +0545

    global error handling middleware

---
+++ src/middleware/middleware.error.js
@@ -0,0 +1,19 @@
+import {ApiResponse} from "../utils/apiResponse.js";
+
+// Global error handler
+export const globalErrorHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json(
+    new ApiResponse(
+      statusCode,
+      err.data || null,
+      err.message || "Internal Server Error",
+      err.errors || []
+    )
+  );
+};
+
+// 404 handler
+export const notFoundHandler = (req, res, next) => {
+  res.status(404).json(new ApiResponse(404, null, "Route not found"));
+};
