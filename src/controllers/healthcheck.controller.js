75f56f2bb1c70cbe3969014c476b18cfa3257ce6
Raman Singh <ramansinghchauhan787@gmail.com>
2025-12-06 15:27:15 +0545

    Add healthCheckController

---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,9 @@
+import {ApiResponse} from '../utils/apiResponse.js';
+import { asyncHandler } from '../utils/asynHandler.js';
+export const healthCheck = asyncHandler(async function(req,res){
+
+    return res.status(200).json(new ApiResponse(200,null,"Server is Runing !!!"))
+
+
+
+})
