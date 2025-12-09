280cbaa0590a5334950b13f4b12cba1c450ba76b
Raman Singh <ramansinghchauhan787@gmail.com>
2025-12-09 12:57:44 +0545

    Fix typo in password field validation and enhance username validation rules

--- src/validator/index.js
+++ src/validator/index.js
@@ -1,14 +1,26 @@
 export const userRegisterValidator = () => {
-  return [body("email")
-    .trim()
-    .notEmpty()
-    .withMessage("Email is required!!")
-    .isEmail()
-    .withMessage("invalid Email"),
-     body("username")
-     .trim()
-     .notEmpty()
-     .withMessage("Username is required!! ")
-
-]
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required!!")
+      .isEmail()
+      .withMessage("invalid Email"),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is required!! ")
+      .isLowercase()
+      .withMessage("username must be in lower case")
+      .isLength()
+      .withMessage("username must have at least 3 characters"),
+      body("passwrod")
+      .trim()
+      .notEmpty()
+      .withMessage("password is required!!"),
+      body("fullname")
+      .optional()
+      .trim()
+  ];
+  
 };
